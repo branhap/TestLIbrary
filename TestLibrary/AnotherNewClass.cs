ef5aa1f7a531eed54d084eabe21edaa29dff0dc1
w2k\paulbranham <paulbranham@truckstop.com>
2018-05-02 17:50:38 -0600

    Added anothernewclass.

---
+++ TestLibrary/AnotherNewClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestLibrary
+{
+    public class AnotherNewClass
+    {
+        public int Id { get; set; }
+        public int Description { get; set; }
+        public MyTestClass TestItem { get; set; }
+    }
+}
